d8cfc8108aed8ba0f817ce623acd04e761295e53
Karina Timofeeva <kar.timofeeva93@gmail.com>
2024-02-02 17:49:39 +0100

    Updated src/locales/en.js

--- src/locales/en.js
+++ src/locales/en.js
@@ -1,28 +1,26 @@
 export default {
-  en: {
-    // Translation resources for English
-    translation: {
-      formTitle: "RSS agregator",
-      formSubtitle: "Your main news is always at hand",
-      placeholder: "link RSS",
-      urlExample: "Exapmle: https://ru.hexlet.io/lessons.rss",
-      rssUploaded: "RSS uploaded successfully",
-      rssIsLoading: "RSS is loading",
-      feedsTitle: "Feeds",
-      postsTitle: "Posts",
-      authorContainer: "created by",
-      buttons: {
-        addUrlBtn: "Add",
-        watchBtn: "View",
-        modalReadBtn: "Read more",
-        modalCloseBtn: "Close",
-      },
-      errors: {
-        rssAlreadyExists: "RSS already exists",
-        invalidUrl: "The link must be a valid URL",
-        errorNetwork: "Network error",
-        noValidRss: "Invalid RSS",
-      },
+  // Translation resources for English
+  translation: {
+    formTitle: "RSS agregator",
+    formSubtitle: "Your main news is always at hand",
+    placeholder: "link RSS",
+    urlExample: "Exapmle: https://ru.hexlet.io/lessons.rss",
+    rssUploaded: "RSS uploaded successfully",
+    rssIsLoading: "RSS is loading",
+    feedsTitle: "Feeds",
+    postsTitle: "Posts",
+    authorContainer: "created by",
+    buttons: {
+      addUrlBtn: "Add",
+      watchBtn: "View",
+      modalReadBtn: "Read more",
+      modalCloseBtn: "Close",
+    },
+    errors: {
+      rssAlreadyExists: "RSS already exists",
+      invalidUrl: "The link must be a valid URL",
+      errorNetwork: "Network error",
+      noValidRss: "The resource does not contain a valid RSS feed",
     },
   },
 };
