088c126568a27752c35ba4050491ae134ee30e1d
Karina Timofeeva <kar.timofeeva93@gmail.com>
2024-02-08 12:02:57 +0100

    Updated src/model.js

--- src/model.js
+++ src/model.js
@@ -1,74 +1,76 @@
 import onChange from 'on-change';
 import * as yup from 'yup';
 import i18next from 'i18next';
-import axios from 'axios';
-import _ from 'lodash';
+// import _ from 'lodash';
 import { renderUIView } from './view.js';
 import ru from './locales/ru.js';
 import en from './locales/en.js';
 import {
-  validateInputValue,
-  parseRSSFeed,
-  generateAxiosGetRequestUrl,
-  generateNewFeedObj,
-  generateNewPostsObjOfFeed,
-  updateExistingRssPostsWithTimer,
-} from './helpers.js';
-import { controlClickedPostLinks, controlModalWindow } from './controller.js';
+  controlClickedPostLinks,
+  controlModalWindow,
+  controlValidationAndAxiosRequest,
+} from './controller.js';
+
+const initializeElements = (i18nInstance) => {
+  const formEl = {
+    formInput: document.querySelector('#url-input'),
+    form: document.querySelector('.rss-form'),
+    addUrlBtn: document.querySelector('.btn-lg'),
+    feedback: document.querySelector('.feedback'),
+    formTitle: document.querySelector('.display-3'),
+    formSubtitle: document.querySelector('.lead'),
+    placeholder: document.querySelector('[for="url-input"]'),
+    urlExample: document.querySelector('.example'),
+  };
+
+  const modalWindowEl = {
+    modalWindow: document.querySelector('.modal'),
+    modalTitle: document.querySelector('.modal-title'),
+    modalDescription: document.querySelector('.modal-body'),
+    modalReadBtn: document.querySelector('.btn-primary.full-article'),
+    modalCloseBtn: document.querySelector('.btn-secondary'),
+  };
+
+  const feedsAndPostsEl = {
+    feedsMainDivContainer: document.querySelector('.feeds'),
+    postsMainDivContainer: document.querySelector('.posts'),
+    h2FeedCardTitle: i18nInstance.t('feedCardTitle'),
+    h2PostCardTitle: i18nInstance.t('postCardTitle'),
+    watchBtn: i18nInstance.t('buttons.watchPostBtn'),
+  };
+
+  const elements = { formEl, modalWindowEl, feedsAndPostsEl };
+
+  return elements;
+};
 
 const defaultLanguage = 'ru';
 
-const i18nInstance = i18next.createInstance();
-i18nInstance.init({
-  lng: defaultLanguage,
-  debug: false,
-  resources: {
-    ru,
-    en,
-  },
-});
-
-yup.setLocale({
-  mixed: {
-    notOneOf: () => ({ key: 'errors.rssAlreadyExists' }),
-  },
-  string: {
-    url: () => ({ key: 'errors.invalidUrl' }),
-  },
-});
-
-const app = () => {
-  const elements = {
-    formEl: {
-      formInput: document.querySelector('#url-input'),
-      form: document.querySelector('.rss-form'),
-      addUrlBtn: document.querySelector('.btn-lg'),
-      feedback: document.querySelector('.feedback'),
-      formTitle: document.querySelector('.display-3'),
-      formSubtitle: document.querySelector('.lead'),
-      placeholder: document.querySelector('[for="url-input"]'),
-      urlExample: document.querySelector('.example'),
+const initializeI18n = () => {
+  const i18nInstance = i18next.createInstance();
+  i18nInstance.init({
+    lng: defaultLanguage,
+    debug: false,
+    resources: {
+      ru,
+      en,
     },
-    modalWindowEl: {
-      modalWindow: document.querySelector('.modal'),
-      modalTitle: document.querySelector('.modal-title'),
-      modalDescription: document.querySelector('.modal-body'),
-      modalReadBtn: document.querySelector('.btn-primary.full-article'),
-      modalCloseBtn: document.querySelector('.btn-secondary'),
-    },
+  });
+  return i18nInstance;
+};
 
-    feedsAndPostsEl: {
-      feedsMainDivContainer: document.querySelector('.feeds'),
-      postsMainDivContainer: document.querySelector('.posts'),
-      h2FeedCardTitle: i18nInstance.t('feedCardTitle'),
-      h2PostCardTitle: i18nInstance.t('postCardTitle'),
-      watchBtn: i18nInstance.t('buttons.watchPostBtn'),
+const initializeYup = () => {
+  yup.setLocale({
+    mixed: {
+      notOneOf: () => ({ key: 'errors.rssAlreadyExists' }),
     },
-    footer: {
-      authorContainer: document.querySelector('.text-center'),
+    string: {
+      url: () => ({ key: 'errors.invalidUrl' }),
     },
-  };
+  });
+};
 
+const initializeState = () => {
   const state = {
     lng: defaultLanguage,
     form: {
@@ -86,66 +88,22 @@ const app = () => {
       modalWindowContent: null,
     },
   };
+  return state;
+};
+
+const initializeApp = () => {
+  const i18nInstance = initializeI18n();
+  const elements = initializeElements(i18nInstance);
+  const state = initializeState();
+  initializeYup();
 
   const watchedState = onChange(state, (pathToEl) => {
     renderUIView(state, i18nInstance, elements)(pathToEl);
   });
 
-  elements.formEl.form.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    const inputUrlByUser = e.target.url.value;
-    watchedState.form.loadingProcess.processState = 'formFilling';
-
-    validateInputValue(watchedState, inputUrlByUser)
-      .then((validUserUrl) => {
-        watchedState.form.loadingProcess.processState = 'completed';
-        watchedState.form.processError = null;
-        watchedState.form.arrOfValidUrls.push(inputUrlByUser);
-        watchedState.form.loadingProcess.processState = 'processingRequest';
-        axios
-          .get(generateAxiosGetRequestUrl(validUserUrl))
-          .then((responseData) => {
-            watchedState.form.loadingProcess.processState = 'completed';
-
-            const uniqueFeedId = _.uniqueId();
-            const parsedResponseData = parseRSSFeed(responseData);
-
-            const feedObj = generateNewFeedObj(
-              parsedResponseData,
-              uniqueFeedId,
-              validUserUrl,
-            );
-
-            const postsObjOfCurrentFeed = generateNewPostsObjOfFeed(
-              parsedResponseData,
-              uniqueFeedId,
-            );
-
-            watchedState.feeds.push(feedObj);
-            watchedState.posts.push(postsObjOfCurrentFeed);
-
-            updateExistingRssPostsWithTimer(watchedState);
-          })
-          .catch((error) => {
-            watchedState.form.loadingProcess.processState = 'responseAndNetworkError';
-            if (error.message === 'The XML document is not well-formed') {
-              watchedState.form.loadingProcess.processError = i18nInstance.t('errors.noValidRss');
-            } else {
-              watchedState.form.loadingProcess.processError = i18nInstance.t(
-                'errors.errorNetwork',
-              );
-            }
-          });
-      })
-      .catch((error) => {
-        watchedState.form.loadingProcess.processState = 'validationError';
-        watchedState.form.validError = i18nInstance.t(error.message.key);
-      });
-  });
-
+  controlValidationAndAxiosRequest(watchedState, elements, i18nInstance);
   controlClickedPostLinks(watchedState, elements);
   controlModalWindow(watchedState, elements);
 };
 
-export default app;
+export default initializeApp;
