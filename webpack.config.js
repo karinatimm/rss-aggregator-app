a388d4fb1e4d61e351a238632ed96d6a6e711ee7
Karina Timofeeva <kar.timofeeva93@gmail.com>
2024-02-09 11:56:50 +0100

    Updated webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,3 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
@@ -25,7 +23,6 @@ const config = {
     rules: [
       {
         test: /\.(js|jsx)$/i,
-        loader: 'babel-loader',
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
@@ -55,60 +52,3 @@ export default () => {
   }
   return config;
 };
-
-// const path = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-// const isProduction = process.env.NODE_ENV === 'production';
-
-// const config = {
-//   entry: './src/index.js',
-//   output: {
-//     path: path.resolve(__dirname, 'dist'),
-//   },
-//   devServer: {
-//     open: true,
-//     host: 'localhost',
-//   },
-//   plugins: [
-//     new HtmlWebpackPlugin({
-//       template: 'index.html',
-//     }),
-
-//     // Add your plugins here
-//     // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-//   ],
-//   module: {
-//     rules: [
-//       {
-//         test: /\.(js|jsx)$/i,
-//         loader: 'babel-loader',
-//       },
-//       {
-//         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-//         type: 'asset',
-//       },
-
-//       // Add your rules for custom modules here
-//       // Learn more about loaders from https://webpack.js.org/loaders/
-//       { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
-//       {
-//         test: /\.scss$/,
-//         use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
-//       },
-//       {
-//         test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-//         use: 'url-loader?limit=10000',
-//       },
-//     ],
-//   },
-// };
-
-// module.exports = () => {
-//   if (isProduction) {
-//     config.mode = 'production';
-//   } else {
-//     config.mode = 'development';
-//   }
-//   return config;
-// };
